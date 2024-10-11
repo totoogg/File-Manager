e0d3261fcc72163d5ea0ebd827ae6394937b10dd
totoogg <goncharov.vova2014@gamil.com>
2024-10-11 15:21:14 +0300

    feat: add module error

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { userPath } from './modules/modulePath.js';
 import { userInfo } from 'os';
 import { logging } from './modules/moduleGreetingAndFarewell.js';
 import { showFolderContent } from './modules/moduleShowFolderContent.js';
+import { invalidError } from './modules/moduleError.js';
 
 const name =
   process.argv[2]?.slice(11) || process.env.npm_config_username || 'Anonyms';
@@ -13,8 +14,8 @@ logging.greeting(printName);
 userPath.setPath(userInfo().homedir);
 
 process.stdin.on('data', (data) => {
-  const command = data.toString().trim();
-  switch (command.split(' ')[0]) {
+  const command = data.toString().trim().split(' ');
+  switch (command[0]) {
     case '.exit':
       process.exit();
       break;
@@ -22,14 +23,53 @@ process.stdin.on('data', (data) => {
       userPath.upPath();
       break;
     case 'cd':
-      userPath.setRootPath(command);
+      userPath.setRootPath(command[1]);
       break;
     case 'ls':
       showFolderContent(userPath.getPath());
       break;
-
+    case 'cat':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'add':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'rn':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'cp':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'mv':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'rm':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'os':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'hash':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'compress':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
+    case 'decompress':
+      showFolderContent(userPath.getPath());
+      userPath.showPath();
+      break;
     default:
-      console.log(data.toString().trim());
+      invalidError();
       break;
   }
 });
--- src/modules/moduleShowFolderContent.js
+++ src/modules/moduleShowFolderContent.js
@@ -1,14 +1,21 @@
 import fs from 'fs/promises';
+import { join } from 'path';
+import { userPath } from './modulePath.js';
 
 export async function showFolderContent(path) {
   const arr = await fs.readdir(path);
   const result = arr.map(async (el) => ({
     Name: el,
-    Type: (await fs.lstat(join(path, el))).isFile()
-      ? 'file'
-      : 'directory',
+    Type: (await fs.lstat(join(path, el))).isFile() ? 'file' : 'directory',
   }));
-  Promise.all(result).then((res) =>
-    console.table(res.sort((a, b) => a.Type.localeCompare(b.Type)))
-  );
+  Promise.all(result).then((res) => {
+    const arrFolders = res
+      .filter((el) => el.Type === 'directory')
+      .sort((a, b) => a.Name.localeCompare(b.Name));
+    const arrFiles = res
+      .filter((el) => el.Type === 'file')
+      .sort((a, b) => a.Name.localeCompare(b.Name));
+    console.table([...arrFolders, ...arrFiles]);
+    userPath.showPath();
+  });
 }
